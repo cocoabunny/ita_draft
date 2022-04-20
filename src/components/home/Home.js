47d0c9ceed219c3b8c6d1aa26951bf33be5bdcda
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-04-20 19:47:38 +1000

    First Commit
    This is the first commit for the ita project

---
+++ src/components/home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const home = () => {
+    return (
+        <div className='home'>
+            <div className='container'>
+                <div className='header'>
+                    <h1>ITA <br>PANELS</br></h1>
+
+                </div>
+                <div className='content'>
+                    <div className='p1'> <h2>Services</h2>
+                        <p>dolor sit amet, consectetur adipiscing elit. Integer placerat congue mauris sed imperdiet. Morbi a facilisis felis. Aenean ut venenatis odio. </p></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
