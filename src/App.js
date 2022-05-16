427519f4bdb0c8be609bed3efda8f84f2407ec04
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-05-16 14:08:21 +1000

    services page blocked in for all breakpoints

--- src/App.js
+++ src/App.js
@@ -38,15 +38,19 @@ function App() {
         <div className="md:bg-slate-700 xl:bg-yellow-300 ">
           {/* <img src="assets/vertical-home-hero.jpg" alt="tools 'n stuff" className="max-w-full h-auto md:hidden lg:hidden" /> */}
           <div className="flex flex-col text-[#ff0000] mx-8">
+            {/* Title */}
             <h1 className="text-4xl mt-[2em] bg-white w-3/5 p-4 md:w-[40vw] md:bg-wide-home xl:text-6xl xl:w-[30vw]">PAGE TITLE</h1>
+            {/* Article */}
             <div className="space-y-1 mt-[7em] md:space-y-8 xl:mt-[5em]">
               <h2 className="pl-4 py-3 text-2xl bg-orange-300 w-[65vw] md:w-[30vw] md:p-2 xl:text-3xl xl:w-[20vw]">Title</h2>
               <p className="h-auto p-2 text-xl bg-cyan-400 w-[80vw] md:w-[30vw] md:ml-[10vw] xl:w-[30vw] xl:ml-[20vw]">SECTION</p>
             </div>
+            {/* Article */}
             <div className="space-y-1 mt-[7em] md:space-y-8 xl:mt-[5em]">
               <h2 className="pl-4 py-3 text-2xl bg-orange-300 w-[65vw] md:w-[30vw] md:p-2 xl:text-3xl xl:w-[20vw]">Title</h2>
               <p className="h-auto p-2 text-xl bg-cyan-400 w-[80vw] md:w-[30vw] md:ml-[10vw] xl:w-[30vw] xl:ml-[20vw]">SECTION</p>
             </div>
+            {/* Article */}
             <div className="space-y-1 mt-[7em] md:space-y-8 xl:mt-[5em]">
               <h2 className="pl-4 py-3 text-2xl bg-orange-300 w-[65vw] md:w-[30vw] md:p-2 xl:text-3xl xl:w-[20vw]">Title</h2>
               <p className="h-auto p-2 text-xl bg-cyan-400 w-[80vw] md:w-[30vw] md:ml-[10vw] xl:w-[30vw] xl:ml-[20vw]">SECTION</p>
@@ -54,6 +58,33 @@ function App() {
           </div>
         </div>
       </main>
+
+      {/* Services Page */}
+      <div className="w-full h-screen bg-slate-400 md:bg-red-200 xl:bg-lime-200">
+        <div className="flex flex-col m-[1.2em]">
+          {/* Title */}
+          <div className="bg-lime-600 md:bg-slate-600 xl:bg-emerald-800 text-4xl pl-[2em] md:py-[.2em] xl:mt-[3em] ">
+            <h1 className="text-white md:pl-[2em]">ICONIC</h1>
+            <h1 className="text-orange-500 pl-[2em] md:pl-[5.2em]">PHRASE</h1>
+          </div>
+          {/* Section Style 1*/}
+          <div className="bg-emerald-800 md:lime-600 xl:slate-600 mt-[22em]  w-2/5 ml-[13em] pl-[2em] md:mt-[27em] xl:w-1/6 xl:mt-[7em] xl:ml-[3em]">
+            <h2 className="text-2xl text-orange-500 align-center md:text-3xl">GORDAN'S</h2>
+          </div>
+          {/* paragraph */}
+          <div className="bg-emerald-400 md:bg-purple-400 xl:bg-rose-400 mt-[.5em] ml-[1.5em] mr-[6em] w-4/5 xl:w-1/6 xl:ml-[3em] xl:p-[1em]">
+            <p className="text-white">ROFL PANELBEATING AM I RITE, CARS AND SHIT</p>
+          </div>
+          {/* Section Style 2*/}
+          <div className="bg-emerald-800 md:lime-600 xl:slate-600 mt-[6em] w-2/5 ml-[1.5em] px-[1.5em] xl:w-1/6 xl:ml-[16.2em]">
+            <h2 className="text-2xl text-orange-500 align-center md:text-3xl">NAUGHYT</h2>
+          </div>
+          {/* paragraph */}
+          <div className="bg-emerald-400 md:bg-purple-400 xl:bg-rose-400 mt-[.5em] ml-[4.5em] w-3/5 md:mb-[.5em] xl:w-1/6 xl:ml-[16.2em] xl:p-[1em]" >
+            <p className="text-white">OMG I'M A STINKY REDHEAD THAT DRINKS COCA COLA, AND LOVES TO HELP OUT CHIMPS FROM CALIFORNIA</p>
+          </div>
+        </div>
+      </div>
 
       {/* Footer */}
       <footer className="bg-gray-900 text-white">
