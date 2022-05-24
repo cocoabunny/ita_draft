72c358223f8fed4a3e9e33a4836a04572512f0e2
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2022-05-24 20:36:42 +1000

    Contact page blocked in

--- src/App.js
+++ src/App.js
@@ -214,42 +214,50 @@ function App() {
       <div className="h-screen w-full flex flex-col text-white bg-zinc-400 md:bg-orange-200 xl:bg-green-200">
         <main className="mx-[3em]">
           {/* Enquiries Header */}
-          <div className="flex flex-col items-center mt-14">
-            <h1 className="bg-blue-400 text-4xl text-yellow-600 font-bold tracking-widest">ENQUIRIES</h1>
-            <p className="bg-cyan-200 ml-[13em] w-3/5 my-4">Please fill out the form below for a quote.</p>
+          <div className="flex flex-col items-center mt-10">
+            <h1 className="bg-blue-400 text-4xl text-yellow-600 font-bold tracking-widest self-start xl:text-5xl xl:font-semibold">ENQUIRIES</h1>
+            <p className="bg-cyan-200 ml-[13em] w-3/5 my-4 md:self-start md:ml-[6em] md:w-1/3 xl:w-1/6 xl:ml-[17.8em]">Please fill out the form below for a quote.</p>
           </div>
           <section>
             {/* Email Form Section */}
-            <form className="flex flex-col items-center p-2 mt-[8em]">
-              <label for="email" className="text-3xl text-yellow-600 font-semibold">EMAIL</label>
+            <form className="flex flex-col items-center p-2 mt-[8em] xl:mt-[6em]">
+              <label for="email" className="text-3xl text-yellow-600 font-semibold bg-emerald-200 tracking-wide self-start md:self-center md:ml-[1.5em] xl:text-4xl xl:self-start xl:ml-0" >EMAIL</label>
               <br />
-              <input type="text" id="email" className=""></input>
+              <input type="text" id="email" className="ml-[9.6em] md:w-2/5 md:self-end xl:self-start xl:ml-[7.7em]"></input>
               <br />
-              <label for="enquiry" className="text-3xl text-yellow-600 font-semibold">ENQUIRY INFO</label>
-              <p className="bg-blue-400 w-3/5 ">Please describe what services interest you and what vehicle you own</p>
-              <textarea type="text" id="enquiry" className=""></textarea>
+              <label for="enquiry" className="text-3xl text-yellow-600  bg-emerald-200 tracking-wide font-semibold mr-[.3em] self-start xl:text-4xl">ENQUIRY INFO</label>
+              <p className="bg-blue-400 w-3/5 ml-[13em] md:mr-[21.3em] md:w-2/5 xl:w-1/4 xl:ml-[2.6em]">Please describe what services interest you and what vehicle you own</p>
+              <textarea type="text" id="enquiry" className="my-4 py-10 md:py-[8em] w-full self-start xl:w-1/2"></textarea>
               <br />
-              <button>Submit</button>
+              <button className="bg-yellow-600 rounded px-2 md:px-12 md:text-2xl md:pb-1 xl:self-start">Submit</button>
             </form>
           </section>
+        </main>
+      </div>
+
+      <div className="h-screen w-full flex flex-col  text-white bg-zinc-400 md:bg-orange-200 xl:bg-green-200 pt-4">
+        <main className="mx-[3em]">
           {/* Address Info Section */}
           <div>
-            <section>
-              <h2></h2>
-              <p></p>
-              <div>
+            <section className="flex flex-col items-start font-semibold">
+              <h2 className="text-4xl xl:5xl text-yellow-600 font-bold xl:font-semibold tracking-wider bg-yellow-200 w-1/2 p-1">ADDRESS</h2>
+              <p className="p-1 bg-cyan-300">Our Shop is located at <br /></p>
+              <p className="text-yellow-600 bg-yellow-200 p-1">Factory 1/3 Hammer Ct,<br />Hoppers Crossing, <br /> Victoria, 3029 </p>
+              <div className="bg-pink-300 w-full py-[12em] mt-[4em] mb-8">
                 {/* Google Maps Embed Container */}
+
               </div>
-              <h3></h3>
-              <p> </p>
-              <div>
+              <h3 className="text-3xl bg-yellow-200">SOCIAL</h3>
+              <p className="w-1/3 p-1 text-yellow-600 bg-cyan-200 md:ml-[6.4em]">Stay up to date with us on instagram and facebook</p>
+              <div className="flex flex-row">
                 {/* Social Media Icons Container */}
+                <button className="bg-yellow-600 p-3 self-start">I</button>
+                <button className="bg-yellow-600 p-3 mx-3 self-center">FB</button>
               </div>
             </section>
           </div>
         </main>
       </div>
-
 
       {/* Footer */}
       <footer className="bg-gray-900 text-white">
