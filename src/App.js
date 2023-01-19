7b9e7f098c9e1e8b1a90a26421268740e4533bde
cocoabunny <65260797+cocoabunny@users.noreply.github.com>
2023-01-19 19:10:35 +1100

    vercel analytics commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,15 @@ import React from "react";
 import Home from "./components/home/Home";
 import Nav from "./components/nav/Nav";
 import Footer from "./components/footer/Footer";
-
-
-
-
-
+import { inject } from "@vercel/analytics";
 
 function App() {
-
+  inject();
   return (
     <div className="flex flex-col min-h-screen">
-
       <Nav />
       <Home />
       <Footer />
-
     </div>
   );
 }
